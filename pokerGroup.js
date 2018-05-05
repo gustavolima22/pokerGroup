118dcb84463bf892a0561e245beea325be0e6abd
Gustavo Lima <gustavo.lima@groupsoftware.com.br>
2018-05-05 10:18:50 -0300

    Cartas na mão dos dois jogadores

--- pokerGroup.js
+++ pokerGroup.js
@@ -12,14 +12,15 @@ while(cartaRetirada.length < 10){
     carta = valorCarta[Math.floor((Math.random() * 13))];
     naipe =  valorNaipe[Math.floor((Math.random() * 4))];
     cartaRetirada.push(carta + naipe);
+    console.log(cartaRetirada);
 }
 for(var i = 0; i < 5; i++){
     jogador1[i] = cartaRetirada[i];
 }
-//for(var i = 5; i < 10; i++){
-//    jogador2[i-5] = cartaRetirada[i-5];
-//}
+for(var i = 5; i < 10; i++){
+    jogador2[i-5] = cartaRetirada[i];
+}
 console.log('Jogador 1: ');
 console.log(jogador1);
-//console.log('Jogador 2: ');
-//console.log(jogador2);
+console.log('Jogador 2: ');
+console.log(jogador2);
