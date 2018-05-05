67af3c377893618179a80ad2dc2a7cd26cc6545f
Gustavo Lima <gustavo.lima@groupsoftware.com.br>
2018-05-05 10:43:54 -0300

    Adicionado o objeto aos jogadores

--- pokerGroup.js
+++ pokerGroup.js
@@ -1,12 +1,33 @@
 'use strict'
 
-var valorCarta = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+var valorCarta = ['2','3','4','5','6','7','8','9','10','J','Q','R','S'];
+//J = Valete    Q = Rainha      R = Rei     S = Ás
 var valorNaipe = ['D','H','S','C'];
+//Ouro (D), Copa (H), Espadas (S), Paus (C)
 var cartaRetirada = [];
-var jogador1 = [];
-var jogador2 = [];
+var jogador1 = {
+    cartas: [],
+    valorMao: null
+};
+var jogador2 = {
+    cartas: [],
+    valorMao: null
+};
 var carta = '';
 var naipe = '';
+
+var regras = {
+    'Carta Alta': 1,
+    'Um Par': 2,
+    'Dois Pares': 3,
+    'Trinca': 4,
+    'Straight': 5,
+    'Flush': 6,
+    'Full House': 7,
+    'Quadra': 8,
+    'Straight Flush': 9,
+    'Royal Flush': 10
+};
 
 while(cartaRetirada.length < 10){
     carta = valorCarta[Math.floor((Math.random() * 13))];
@@ -15,12 +36,13 @@ while(cartaRetirada.length < 10){
     console.log(cartaRetirada);
 }
 for(var i = 0; i < 5; i++){
-    jogador1[i] = cartaRetirada[i];
+    jogador1.cartas[i] = cartaRetirada[i];
 }
 for(var i = 5; i < 10; i++){
-    jogador2[i-5] = cartaRetirada[i];
+    jogador2.cartas[i-5] = cartaRetirada[i];
 }
 console.log('Jogador 1: ');
 console.log(jogador1);
 console.log('Jogador 2: ');
 console.log(jogador2);
+
