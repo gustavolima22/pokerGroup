4b85d09013d8f6a89b19b5b2cc8552a966a89b5e
Gustavo Lima <gustavo.lima@groupsoftware.com.br>
2018-05-05 11:47:44 -0300

    Validação do royal flush

--- pokerGroup.js
+++ pokerGroup.js
@@ -42,6 +42,7 @@ for(var i = 0; i < 5; i++){
 for(var i = 5; i < 10; i++){
     jogador2.cartas[i-5] = cartaRetirada[i];
 }
+jogador1.cartas = [ '10C', 'JC', 'QC', 'RC', 'SC' ];
 console.log('Jogador 1: ');
 console.log(jogador1.cartas);
 console.log('Jogador 2: ');
@@ -64,6 +65,57 @@ function isFlush(playerHand){
         return 1; //Carta alta
     }else{
         return 6; //Flush
+    }
+}
+
+function sequencia(playerHand){
+    var possuiCarta = {
+        dois: false,
+        tres: false,
+        quatro: false,
+        cinco: false,
+        seis: false,
+        sete: false,
+        oito: false,
+        nove: false,
+        dez: false,
+        valete: false,
+        dama: false,
+        rei: false,
+        as: false
+    }
+    for(var i = 0; i < 5; i++){
+        possuiCarta.dois = playerHand[i].search('2') === 0;
+        possuiCarta.tres = playerHand[i].search('3') === 0;
+        possuiCarta.quatro = playerHand[i].search('4') === 0;
+        possuiCarta.cinco = playerHand[i].search('5') === 0;
+        possuiCarta.seis = playerHand[i].search('6') === 0;
+        possuiCarta.sete = playerHand[i].search('7') === 0;
+        possuiCarta.oito = playerHand[i].search('8') === 0;
+        possuiCarta.nove = playerHand[i].search('9') === 0;
+        possuiCarta.dez = playerHand[i].search('10') === 0;
+        possuiCarta.valete = playerHand[i].search('J') === 0;
+        possuiCarta.dama = playerHand[i].search('Q') === 0;
+        possuiCarta.rei = playerHand[i].search('R') === 0;
+        possuiCarta.as = playerHand[i].search('S') === 0;
+    }
+    if(possuiCarta.dez && possuiCarta.valete && possuiCarta.dama && possuiCarta.rei && possuiCarta.as){// Royal flush
+        return 10; //Caso a sequencia seja de dez até ás ele é um Royal Flush
+    }else{
+        var sequenciaAs = possuiCarta.as && possuiCarta.dois && possuiCarta.tres && possuiCarta.quatro && possuiCarta.cinco;
+        var sequenciaDois = possuiCarta.dois && possuiCarta.tres && possuiCarta.quatro && possuiCarta.cinco && possuiCarta.seis;
+        var sequenciaTres = possuiCarta.tres && possuiCarta.quatro && possuiCarta.cinco && possuiCarta.seis && possuiCarta.sete;
+        var sequenciaQuatro = possuiCarta.quatro && possuiCarta.cinco && possuiCarta.seis && possuiCarta.sete && possuiCarta.oito;
+        var sequenciaCinco = possuiCarta.cinco && possuiCarta.seis && possuiCarta.sete && possuiCarta.oito && possuiCarta.nove;
+        var sequenciaSeis = possuiCarta.seis && possuiCarta.sete && possuiCarta.oito && possuiCarta.nove && possuiCarta.dez;
+        var sequenciaSete = possuiCarta.sete && possuiCarta.oito && possuiCarta.nove && possuiCarta.dez && possuiCarta.valete;
+        var sequenciaOito = possuiCarta.oito && possuiCarta.nove && possuiCarta.dez && possuiCarta.valete && possuiCarta.dama;
+        var sequenciaNove = possuiCarta.nove && possuiCarta.dez && possuiCarta.valete && possuiCarta.dama && possuiCarta.rei;
+        if(sequenciaAs || sequenciaDois || sequenciaTres || sequenciaQuatro || sequenciaCinco || sequenciaSeis || sequenciaSete || sequenciaOito || sequenciaNove){
+            return 9; //Caso qualquer sequencia for verdadeira ele tem um Straight Flush
+        }else{
+            return 6; //Caso nã seja sequencia, ele é um flush
+        }
     }
 }
 
