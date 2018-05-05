9f11623b1d57015857aa232bfc00e8cb611b7ed5
Gustavo Lima <gustavo.lima@groupsoftware.com.br>
2018-05-05 11:17:33 -0300

    Função para avaliar se existe flush
    Foi feito a clonagem do array dos naipes, para forçar a vitória do jogador dois.

--- pokerGroup.js
+++ pokerGroup.js
@@ -3,6 +3,7 @@
 var valorCarta = ['2','3','4','5','6','7','8','9','10','J','Q','R','S'];
 //J = Valete    Q = Rainha      R = Rei     S = Ás
 var valorNaipe = ['D','H','S','C'];
+var valorNaipe2 = ['D','D','D','D'];
 //Ouro (D), Copa (H), Espadas (S), Paus (C)
 var cartaRetirada = [];
 var jogador1 = {
@@ -17,21 +18,21 @@ var carta = '';
 var naipe = '';
 
 var regras = {
-    'Carta Alta': 1,
-    'Um Par': 2,
-    'Dois Pares': 3,
-    'Trinca': 4,
-    'Straight': 5,
-    'Flush': 6,
-    'Full House': 7,
-    'Quadra': 8,
-    'Straight Flush': 9,
-    'Royal Flush': 10
+    cartaAlta: 1,
+    umPar: 2,
+    doisPares: 3,
+    trinca: 4,
+    straight: 5,
+    flush: 6,
+    fullHouse: 7,
+    quadra: 8,
+    straightFlush: 9,
+    royalFlush: 10
 };
 
 while(cartaRetirada.length < 10){
     carta = valorCarta[Math.floor((Math.random() * 13))];
-    naipe =  valorNaipe[Math.floor((Math.random() * 4))];
+    naipe = cartaRetirada.length < 5 ? valorNaipe[Math.floor((Math.random() * 4))] : valorNaipe2[Math.floor((Math.random() * 4))];
     cartaRetirada.push(carta + naipe);
     console.log(cartaRetirada);
 }
@@ -48,25 +49,22 @@ console.log(jogador2.cartas);
 
 function setHandValue(player){
     player.valorMao = 1;
-    /* if(){
+    player.valorMao = isFlush(player.cartas);
+}
 
-    }else if(){
-
-    }else if(){
-        
-    }else if(){
-        
-    }else if(){
-        
-    }else if(){
-        
-    }else if(){
-        
-    }else if(){
-        
-    }else if(){
-        
-    } */
+function isFlush(playerHand){
+    var naipe = playerHand[0].substr(-1,1);
+    var naipeDiferente = false;
+    for(var i = 1; i < 5; i++){
+        if(playerHand[i].substr(-1,1) !== naipe)
+            naipeDiferente = true;
+    }
+    
+    if(naipeDiferente){
+        return 1;
+    }else{
+        return 6;
+    }
 }
 
 function comparaMaosJogadores(player1, player2){
